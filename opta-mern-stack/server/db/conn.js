323f72c0687f9fc2f9695b70fe5aec01c624ed03
Sachin Agarwal <sachin@bigbitbus.com>
2022-01-11 14:14:36 -0500

    Server working

--- opta-mern-stack/server/db/conn.js
+++ opta-mern-stack/server/db/conn.js
@@ -1,5 +1,31 @@
 const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
+function conn_string() {
+  MONGODB_URI = process.env.MONGODB_URI
+  MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
+  MONGODB_USER = process.env.MONGODB_USER
+  DATABASE_NAME = process.env.DATABASE_NAME
+
+//  │ mongodb+srv://opta-mernbackend-mongod.dmw9f.mongodb.net undefined undefined myFirstDatabase                                                                                 │
+
+  console.log(MONGODB_URI, MONGODB_USER, MONGODB_PASSWORD, DATABASE_NAME)
+  parts = MONGODB_URI.split("//")
+  if (DATABASE_NAME != "") {
+    rtn_str = [
+      parts[0], 
+      MONGODB_USER + ":" + MONGODB_PASSWORD + "@" + parts[1] + "/" + DATABASE_NAME + + "?retryWrites=true&w=majority" 
+    ].join("//")
+  }
+  else {
+    rtn_str = [
+      parts[0],
+      MONGODB_USER + ":" + MONGODB_PASSWORD + "@" + parts[1]
+  ].join("//")
+  }
+  console.log(rtn_str)
+  return rtn_str
+
+}
+const Db = conn_string();
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
